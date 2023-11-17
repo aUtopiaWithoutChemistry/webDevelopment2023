2b508e17795cfb9986316b900bb5b655cfde86d4
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2023-11-17 13:43:32 -0500

    learnt about the whole process from user's request to middleware to request handler

---
+++ Back-End/3.5 Secrets Project/index.js
@@ -0,0 +1,38 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+var password;
+
+function getPassword(req, res, next) {
+    console.log(req.body);
+    password = req.body["password"];
+    next();
+  }
+
+app.use(bodyParser.urlencoded({ extended: true }));  
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.use(getPassword);
+
+app.post('/check', (req, res) => {
+    if (password === "ILoveProgramming") {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
